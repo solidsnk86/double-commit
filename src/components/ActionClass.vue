ae74cc87a579ca016b14a8cdc6e0892de691fd7f
Gabriel Calcagni <calcagni.gabriel86@gmail.com>
2024-11-11 01:37:20 -0300

    updated try catch in Model class

--- src/components/ActionClass.vue
+++ src/components/ActionClass.vue
@@ -4,14 +4,18 @@ import { z } from 'zod'
 
 export class Model {
   static async getComment() {
-    const { data, error } = await supabase
-      .from('portfolio_comments')
-      .select('*')
-      .limit(10)
-      .order('created_at', { ascending: false })
+    try {
+      const { data, error } = await supabase
+        .from('portfolio_comments')
+        .select('*')
+        .limit(10)
+        .order('created_at', { ascending: false })
 
-    if (error) return console.error('Error al obtener los comentarios', error.message)
-    return data
+      if (error) return console.error('Error al obtener los comentarios', error.message)
+      return data
+    } catch (err) {
+      console.error('Error al obtener comentarios: ', err)
+    }
   }
 
   static async sendComment(schema, data, errors, onCreate) {
@@ -37,33 +41,55 @@ export class Model {
   }
 
   static async sendVisit(obj) {
-    const { data, error } = await supabase.from('double_commit_visits').insert([obj])
-    if (error) {
-      console.error('Error al enviar datos: ', error.message)
+    try {
+      const { data, error } = await supabase.from('double_commit_visits').insert([obj])
+      if (error) {
+        console.error('Error al enviar datos: ', error.message)
+      }
+      return data
+    } catch (err) {
+      console.error('Error al enviar la visita: ', err)
     }
-    return data
   }
 
   static async getVisits({ column }, limit) {
-    const { data, error } = await supabase
-      .from('double_commit_visits')
-      .select(column)
-      .order('id', { ascending: false })
-      .limit(limit)
+    try {
+      const { data, error } = await supabase
+        .from('double_commit_visits')
+        .select(column)
+        .order('id', { ascending: false })
+        .limit(limit)
 
-    if (error) return console.error('Error al recibir los datos de la visita: ', error.message)
-    return data
+      if (error) return console.error('Error al recibir los datos de la visita: ', error.message)
+      return data
+    } catch (err) {
+      console.error('Error al obtener las visitas: ', err)
+    }
+  }
+
+  static async update(id, { content }) {
+    try {
+      const { error } = await supabase.from('portfolio_comments').update(content).match(id)
+
+      if (error) return console.error('Error al actualizar comentarios', error.message)
+    } catch (err) {
+      console.error('Error al actualizar comentario: ', err)
+    }
   }
 
   static async delete(id) {
-    const { error } = await supabase.from('portfolio_comments').delete().eq('id', id)
+    try {
+      const { error } = await supabase.from('portfolio_comments').delete().eq('id', id)
 
-    if (error) {
-      console.error('Error al borrar comentario: ', error.message)
-      return
+      if (error) {
+        console.error('Error al borrar comentario: ', error.message)
+        return
+      }
+
+      return { success: true }
+    } catch (err) {
+      console.error('Error al eliminar comentario: ', err)
     }
-
-    return { success: true }
   }
 }
 </script>
